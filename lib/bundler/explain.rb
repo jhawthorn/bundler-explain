5193bd72938a23e84ce3f9f94e629ef4ee77089c
John Hawthorn <john@hawthorn.email>
2018-06-23 14:38:04 -0700

    Initial commit

---
+++ lib/bundler/explain.rb
@@ -0,0 +1,7 @@
+require "bundler/explain/version"
+
+module Bundler
+  module Explain
+    # Your code goes here...
+  end
+end
---
+++ lib/bundler/explain/version.rb
@@ -0,0 +1,5 @@
+module Bundler
+  module Explain
+    VERSION = "0.1.0"
+  end
+end
